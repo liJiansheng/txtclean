65c96babfc584af5aa0fbe4b589a9d1c12099110
LiJiansheng <leekinsung@gmail.com>
2020-03-11 08:08:49 +0800

    clean txt

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import pandas as pd
 from flask import Flask, jsonify, request,json
-import pickle
-
-model = pickle.load(open('model.pkl','rb'))
 
 app = Flask(__name__)
 
@@ -14,17 +11,58 @@ def predict():
     data = body_dict['0']
     # predictions
 
-    prediction=[]
-    for v in data.values():
-        p=model.predict([v]).tolist()
-        #print(p)
-        prediction.append(p[0])
-#prediction = model.predict([data['0']]).tolist()
-        #print(prediction)
-    result = {'prediction': prediction}   
+    txt=""
+    t=""
+    txtList=data['content']
+    clean_content=[]
+    scrape_txt={}
 
-    # return data
-    return jsonify(prediction)
+    scrape_txt['content']=[c.lower() for c in txtList]
+    for content in scrape_txt['content']:
+        # Convert posts to words, then append to clean_train_content.
+        clean_content.append(review_to_words(content))
+
+    tfid_vectorizer = TfidfVectorizer(max_df=.8,ngram_range=(1,2))
+# Fit and transform the processed titles
+    count_data = tfid_vectorizer.fit_transform(scrape_txt['content'])    
+
+    r = requests.post(url = "https://news-model.herokuapp.com/", data = count_data) 
+# S3 Connect
+    #s3 = boto3.client('s3')
+
+    # Uploaded File
+    #s3.put_object(Bucket=BUCKET_NAME, Key=FILE_NAME, Body=txt)
+
+    return r
+    
+def review_to_words(raw_content):
+    # Function to convert a raw review to a string of words
+    # The input is a single string (a raw movie review), and 
+    # the output is a single string (a preprocessed movie review)
+    
+    # 1. Remove HTML.
+    content_text = BeautifulSoup(raw_content).get_text()
+    
+    # 2. Remove non-letters.
+    letters_only = re.sub("[^a-zA-Z]", " ", content_text)
+    
+    # 3. Convert to lower case, split into individual words.
+    words = letters_only.lower().split()
+    
+    # 4. In Python, searching a set is much faster than searching
+    # a list, so convert the stop words to a set.
+    stops = stopwords.words('english')
+    stops.extend(['http','https','www','com','abcnews','rte','cnn','huffingtonpost','news','bbc','tass','dw','aljeezra','chinadaily','ie','go','politics','said','say','one','would','year','pm', 'nbcsn', 'csn', 'et', 'pt', 'ct', 'ht', 'mt','like','first','two','get'])
+  
+    # 5. Remove stop words.
+    meaningful_words = [w for w in words if not w in stops]
+    # 6. Lemmatize our words
+    lemmatizer = WordNetLemmatizer()
+    tokens_lem = [lemmatizer.lemmatize(i) for i in meaningful_words]
+    
+    # 7. Join the words back into one string separated by space, 
+    # and return the result.
+    return(" ".join(tokens_lem))
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
