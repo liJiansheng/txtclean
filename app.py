fc2fef82c63b1a7c626ac7d852e4e20c6fed955b
LiJiansheng <leekinsung@gmail.com>
2020-03-11 15:26:27 +0800

    f

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def predict():
     # get data
    
     body_dict = json.loads(request.get_data().decode('utf-8')) 
-    data = body_dict['0']
+    #data = body_dict['content']
     # predictions
 
     txt=""
@@ -39,7 +39,7 @@ def predict():
     # Uploaded File
     #s3.put_object(Bucket=BUCKET_NAME, Key=FILE_NAME, Body=txt)
 
-    return data
+    return body_dict
     
 def review_to_words(raw_content):
     # Function to convert a raw review to a string of words
