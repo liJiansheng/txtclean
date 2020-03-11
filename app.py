747ee44ed30d8814281e90a3649df9c6eba965ba
LiJiansheng <leekinsung@gmail.com>
2020-03-12 05:36:41 +0800

    f

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def predict():
     # Uploaded File
     #s3.put_object(Bucket=BUCKET_NAME, Key=FILE_NAME, Body=txt)
 
-    return data.values()
+    return data
     
 def review_to_words(raw_content):
     # Function to convert a raw review to a string of words
