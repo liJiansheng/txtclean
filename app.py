b8d80215445e07419cc463ef89d6c90c84f1abc8
LiJiansheng <leekinsung@gmail.com>
2020-03-11 19:17:31 +0800

    f

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 def predict():
     # get data
    
-    body_dict = request.json()
+    body_dict = request.get_json()
     #data = body_dict['content']
     # predictions
 
